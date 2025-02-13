ea5601035e7060905ed543ff50b92fac04efa8ed
dydydebondy <dylanyehouenou344@gmail.com>
2025-02-13 01:51:33 +0100

    first commit dydy

---
+++ script.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".text", {
+    strings: [" Dévellopeur Web en freelance", "Dévellopeur Swift UI en freelance ", "Futur specialiste en Cybersecurité ","BTS CIEL en Alternance"],
+    typeSpeed: 20,
+    backSpeed: 60,
+    backDelay: 1000,
+    loop: true
+});
