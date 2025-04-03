ec05a41c0775b2bca83b21590d6d4015aaccffc4
dydydebondy <dylanyehouenou344@gmail.com>
2025-04-03 13:40:26 +0200

    new portfolio

--- script.js
+++
@@ -1,7 +0,0 @@
-var typed = new Typed(".text", {
-    strings: [" Développeur  Web en freelance", "Développeur Swift UI en freelance ", "Futur specialiste en Cybersecurité ","BTS CIEL en Alternance", "Développeur full stack en Alternance"],
-    typeSpeed: 20,
-    backSpeed: 60,
-    backDelay: 1000,
-    loop: true
-});
